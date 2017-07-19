25deddd3cb90a6cd0b61b234667164aea09eba43
谷本　祐一 <ytanimot@MBP-15UAC-084.local>
2017-07-20 01:17:31 +0900

    fix

--- src/manager/tournament_manager.go
+++ src/manager/tournament_manager.go
@@ -12,16 +12,17 @@ import (
 )
 
 type TournamentManager struct {
-	Tables []table.Table
+	Tables               []table.Table
+	TournamentDirectorID int
 }
 
 // リクエストで指定されたテーブルに値をbodyのjson通りにセットする
 // もし指定のTable IDが存在しなければエラーを返す
 // この関数が正しく動作するためにはTable IDは全てユニークでなければならない
-func (td *TournamentManager) setTableAsRequested(tableReq request.DealerRequest) error {
-	for i, _ := range td.Tables {
-		if td.Tables[i].ID == tableReq.ID {
-			td.Tables[i].PlayersNum = tableReq.PlayersNum
+func (tm *TournamentManager) setTableAsRequested(tableReq request.DealerRequest) error {
+	for i, _ := range tm.Tables {
+		if tm.Tables[i].ID == tableReq.ID {
+			tm.Tables[i].PlayersNum = tableReq.PlayersNum
 			return nil
 		}
 	}
@@ -29,19 +30,19 @@ func (td *TournamentManager) setTableAsRequested(tableReq request.DealerRequest)
 }
 
 // tableBalance
-func (td *TournamentManager) tableBalance() {
-	minTable, maxTable := &td.Tables[0], &td.Tables[0]
+func (tm *TournamentManager) tableBalance() {
+	minTable, maxTable := &tm.Tables[0], &tm.Tables[0]
 
 	// TODO: 大した計算量ないからひとまずは愚直に計算する
 	//       もっと効率化はできるがやるならバグに気をつけないといけないから費用対効果としては微妙かも。十分速いし
 	for {
-		for i, _ := range td.Tables {
-			if minTable.PlayersNum > td.Tables[i].PlayersNum {
-				minTable = &td.Tables[i]
+		for i, _ := range tm.Tables {
+			if minTable.PlayersNum > tm.Tables[i].PlayersNum {
+				minTable = &tm.Tables[i]
 			}
 
-			if maxTable.PlayersNum < td.Tables[i].PlayersNum {
-				maxTable = &td.Tables[i]
+			if maxTable.PlayersNum < tm.Tables[i].PlayersNum {
+				maxTable = &tm.Tables[i]
 			}
 		}
 
@@ -54,7 +55,7 @@ func (td *TournamentManager) tableBalance() {
 	}
 }
 
-func (td *TournamentManager) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+func (tm *TournamentManager) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	if req.Method != http.MethodPost {
 		w.Write([]byte("Only takes POST request"))
 		return
@@ -76,24 +77,24 @@ func (td *TournamentManager) ServeHTTP(w http.ResponseWriter, req *http.Request)
 	//       でないと例えばtableIDが1, 2, 3, 4とあって2がクローズされた後にまたtableが追加されたら既に存在しているtableID=4が再度出来上がる
 	//       バグを仕込む可能性が高そうな箇所である
 	if tableReq.ID == 0 {
-		tableID := len(td.Tables) + 1
-		td.Tables = append(td.Tables, table.NewTable(tableID, tableReq.PlayersNum))
+		tableID := len(tm.Tables) + 1
+		tm.Tables = append(tm.Tables, table.NewTable(tableID, tableReq.PlayersNum))
 
 		// TODO: ここでクライアント側にtableIDを返す処理が必要
 		fmt.Fprintf(w, "Your tableID is %v\n", tableID)
-	} else if len(td.Tables) > 0 {
-		if err = td.setTableAsRequested(tableReq); err != nil {
+	} else if len(tm.Tables) > 0 {
+		if err = tm.setTableAsRequested(tableReq); err != nil {
 			fmt.Fprintln(w, "Requested ID does not exist")
 			return
 		}
-		td.tableBalance()
+		tm.tableBalance()
 	} else {
 		w.Write([]byte("No table is set yet\n"))
 		return
 	}
 
 	// debug
-	for _, v := range td.Tables {
-		fmt.Fprintf(w, "td: {table_id:%v, players_num:%v}\n", v.ID, v.PlayersNum)
+	for _, v := range tm.Tables {
+		fmt.Fprintf(w, "tm: {table_id:%v, players_num:%v}\n", v.ID, v.PlayersNum)
 	}
 }
