a11fe1b59603b7fd3907207340804a536c61ce8a
谷本　祐一 <ytanimot@MBP-15UAC-084.local>
2017-07-20 01:25:19 +0900

    fix

--- src/manager/tournament_manager.go
+++ src/manager/tournament_manager.go
@@ -19,10 +19,10 @@ type TournamentManager struct {
 // リクエストで指定されたテーブルに値をbodyのjson通りにセットする
 // もし指定のTable IDが存在しなければエラーを返す
 // この関数が正しく動作するためにはTable IDは全てユニークでなければならない
-func (tm *TournamentManager) setTableAsRequested(tableReq request.DealerRequest) error {
+func (tm *TournamentManager) setTableAsRequested(clientReq request.DealerRequest) error {
 	for i, _ := range tm.Tables {
-		if tm.Tables[i].ID == tableReq.ID {
-			tm.Tables[i].PlayersNum = tableReq.PlayersNum
+		if tm.Tables[i].ID == clientReq.ID {
+			tm.Tables[i].PlayersNum = clientReq.PlayersNum
 			return nil
 		}
 	}
@@ -67,8 +67,8 @@ func (tm *TournamentManager) ServeHTTP(w http.ResponseWriter, req *http.Request)
 		return
 	}
 
-	var tableReq request.DealerRequest
-	if err = json.Unmarshal(reqBody, &tableReq); err != nil {
+	var clientReq request.DealerRequest
+	if err = json.Unmarshal(reqBody, &clientReq); err != nil {
 		w.Write([]byte("Request Body is not a proper json format\n"))
 		return
 	}
@@ -76,14 +76,14 @@ func (tm *TournamentManager) ServeHTTP(w http.ResponseWriter, req *http.Request)
 	// TODO: tableIDを付ける規則は改めて修正する必要がある
 	//       でないと例えばtableIDが1, 2, 3, 4とあって2がクローズされた後にまたtableが追加されたら既に存在しているtableID=4が再度出来上がる
 	//       バグを仕込む可能性が高そうな箇所である
-	if tableReq.ID == 0 {
+	if clientReq.ID == 0 {
 		tableID := len(tm.Tables) + 1
-		tm.Tables = append(tm.Tables, table.NewTable(tableID, tableReq.PlayersNum))
+		tm.Tables = append(tm.Tables, table.NewTable(tableID, clientReq.PlayersNum))
 
 		// TODO: ここでクライアント側にtableIDを返す処理が必要
 		fmt.Fprintf(w, "Your tableID is %v\n", tableID)
 	} else if len(tm.Tables) > 0 {
-		if err = tm.setTableAsRequested(tableReq); err != nil {
+		if err = tm.setTableAsRequested(clientReq); err != nil {
 			fmt.Fprintln(w, "Requested ID does not exist")
 			return
 		}
