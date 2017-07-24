cebf5f951dd7da600a58e04b79f8b3447f949f5b
谷本　祐一 <ytanimot@MBP-15UAC-084.local>
2017-07-24 18:39:44 +0900

    fix

--- src/manager/tournament_manager.go
+++ src/manager/tournament_manager.go
@@ -11,18 +11,18 @@ import (
 )
 
 type TournamentManager struct {
-	Tables               []table.Table
-	TournamentDirectorID int
-	RequestJson          *simplejson.Json
+	tables               []table.Table
+	tournamentDirectorID int
+	requestJson          *simplejson.Json
 }
 
 // リクエストで指定されたテーブルに値をbodyのjson通りにセットする
 // もし指定のTable IDが存在しなければエラーを返す
 // この関数が正しく動作するためにはTable IDは全てユニークでなければならない
 func (tm *TournamentManager) setTableAsRequested(requestedTableId int, requestedPlayersNum int) error {
-	for i, _ := range tm.Tables {
-		if tm.Tables[i].ID == requestedTableId {
-			tm.Tables[i].PlayersNum = requestedPlayersNum
+	for i, _ := range tm.tables {
+		if tm.tables[i].ID == requestedTableId {
+			tm.tables[i].PlayersNum = requestedPlayersNum
 			return nil
 		}
 	}
@@ -31,18 +31,18 @@ func (tm *TournamentManager) setTableAsRequested(requestedTableId int, requested
 
 // tableBalance
 func (tm *TournamentManager) tableBalance() {
-	minTable, maxTable := &tm.Tables[0], &tm.Tables[0]
+	minTable, maxTable := &tm.tables[0], &tm.tables[0]
 
 	// TODO: 大した計算量ないからひとまずは愚直に計算する
 	//       もっと効率化はできるがやるならバグに気をつけないといけないから費用対効果としては微妙かも。十分速いし
 	for {
-		for i, _ := range tm.Tables {
-			if minTable.PlayersNum > tm.Tables[i].PlayersNum {
-				minTable = &tm.Tables[i]
+		for i, _ := range tm.tables {
+			if minTable.PlayersNum > tm.tables[i].PlayersNum {
+				minTable = &tm.tables[i]
 			}
 
-			if maxTable.PlayersNum < tm.Tables[i].PlayersNum {
-				maxTable = &tm.Tables[i]
+			if maxTable.PlayersNum < tm.tables[i].PlayersNum {
+				maxTable = &tm.tables[i]
 			}
 		}
 
@@ -69,13 +69,13 @@ func parseJsonRequest(httpReq *http.Request) (*simplejson.Json, error) {
 }
 
 func (tm *TournamentManager) handleDealerRequest(w http.ResponseWriter) {
-	tableId, err := tm.RequestJson.Get("ID").Int()
+	tableId, err := tm.requestJson.Get("ID").Int()
 	if err != nil {
 		fmt.Println("Failed to assert key \"ID\"")
 		return
 	}
 
-	playersNum, err := tm.RequestJson.Get("PlayersNum").Int()
+	playersNum, err := tm.requestJson.Get("PlayersNum").Int()
 	if err != nil {
 		fmt.Println("Failed to assert key \"PlayersNum\"")
 		return
@@ -85,12 +85,12 @@ func (tm *TournamentManager) handleDealerRequest(w http.ResponseWriter) {
 	//       でないと例えばtableIdが1, 2, 3, 4とあって2がクローズされた後にまたtableが追加されたら既に存在しているtableId=4が再度出来上がる
 	//       バグを仕込む可能性が高そうな箇所である
 	if tableId == 0 {
-		tableId = len(tm.Tables) + 1
-		tm.Tables = append(tm.Tables, table.NewTable(tableId, playersNum))
+		tableId = len(tm.tables) + 1
+		tm.tables = append(tm.tables, table.NewTable(tableId, playersNum))
 
 		// TODO: ここでクライアント側にtableIdを返す処理が必要
 		fmt.Fprintf(w, "Your tableId is %v\n", tableId)
-	} else if len(tm.Tables) > 0 {
+	} else if len(tm.tables) > 0 {
 		if err = tm.setTableAsRequested(tableId, playersNum); err != nil {
 			fmt.Fprintln(w, "Requested ID does not exist")
 			return
@@ -102,28 +102,34 @@ func (tm *TournamentManager) handleDealerRequest(w http.ResponseWriter) {
 	}
 }
 
+// TODO: これから実装
+func (tm *TournamentManager) handleTournamentDirectorRequest() {
+	if tm.tournamentDirectorID == 0 {
+	}
+}
+
 func (tm *TournamentManager) ServeHTTP(w http.ResponseWriter, httpReq *http.Request) {
 	// TDとDealerのAPIは統一するべきかよく考える必要がある
 	var err error
-	tm.RequestJson, err = parseJsonRequest(httpReq)
+	tm.requestJson, err = parseJsonRequest(httpReq)
 	if err != nil {
 		fmt.Fprintln(w, "Failed to parse json request")
 		return
 	}
 
-	isTdRequest, err := tm.RequestJson.Get("IsTDRequest").Bool()
+	isTdRequest, err := tm.requestJson.Get("IsTDRequest").Bool()
 	if err != nil {
 		fmt.Fprintln(w, "Failed to assert key \"IsTDRequest\"")
 	}
 
 	if isTdRequest == true {
-		//tm.handleTournamentDirectorRequest()
+		tm.handleTournamentDirectorRequest()
 	} else {
 		tm.handleDealerRequest(w)
 	}
 
 	// debug
-	for _, v := range tm.Tables {
+	for _, v := range tm.tables {
 		fmt.Fprintf(w, "tm: {table_id:%v, players_num:%v}\n", v.ID, v.PlayersNum)
 	}
 }
