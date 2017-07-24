36e34678b18f9f926cda2a34c8842d6ebe0fd7bc
谷本　祐一 <ytanimot@MBP-15UAC-084.local>
2017-07-24 18:27:11 +0900

    devide to server dealer and td request

--- src/manager/tournament_manager.go
+++ src/manager/tournament_manager.go
@@ -2,11 +2,10 @@ package manager
 
 import (
 	// 相対パスよくないけど暫定で使う
-	"../request"
 	"../table"
-	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/bitly/go-simplejson"
 	"io/ioutil"
 	"net/http"
 )
@@ -14,15 +13,16 @@ import (
 type TournamentManager struct {
 	Tables               []table.Table
 	TournamentDirectorID int
+	RequestJson          *simplejson.Json
 }
 
 // リクエストで指定されたテーブルに値をbodyのjson通りにセットする
 // もし指定のTable IDが存在しなければエラーを返す
 // この関数が正しく動作するためにはTable IDは全てユニークでなければならない
-func (tm *TournamentManager) setTableAsRequested(clientReq request.DealerRequest) error {
+func (tm *TournamentManager) setTableAsRequested(requestedTableId int, requestedPlayersNum int) error {
 	for i, _ := range tm.Tables {
-		if tm.Tables[i].ID == clientReq.ID {
-			tm.Tables[i].PlayersNum = clientReq.PlayersNum
+		if tm.Tables[i].ID == requestedTableId {
+			tm.Tables[i].PlayersNum = requestedPlayersNum
 			return nil
 		}
 	}
@@ -55,35 +55,43 @@ func (tm *TournamentManager) tableBalance() {
 	}
 }
 
-func (tm *TournamentManager) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	if req.Method != http.MethodPost {
-		w.Write([]byte("Only takes POST request"))
-		return
+func parseJsonRequest(httpReq *http.Request) (*simplejson.Json, error) {
+	if httpReq.Method != http.MethodPost {
+		return nil, errors.New("Only takes POST request\n")
 	}
 
-	reqBody, err := ioutil.ReadAll(req.Body)
+	reqBody, err := ioutil.ReadAll(httpReq.Body)
 	if err != nil {
-		w.Write([]byte("Failed to read request body\n"))
+		return nil, errors.New("Failed to read request body\n")
+	}
+
+	return simplejson.NewJson(reqBody)
+}
+
+func (tm *TournamentManager) handleDealerRequest(w http.ResponseWriter) {
+	tableId, err := tm.RequestJson.Get("ID").Int()
+	if err != nil {
+		fmt.Println("Failed to assert key \"ID\"")
 		return
 	}
 
-	var clientReq request.DealerRequest
-	if err = json.Unmarshal(reqBody, &clientReq); err != nil {
-		w.Write([]byte("Request Body is not a proper json format\n"))
+	playersNum, err := tm.RequestJson.Get("PlayersNum").Int()
+	if err != nil {
+		fmt.Println("Failed to assert key \"PlayersNum\"")
 		return
 	}
 
-	// TODO: tableIDを付ける規則は改めて修正する必要がある
-	//       でないと例えばtableIDが1, 2, 3, 4とあって2がクローズされた後にまたtableが追加されたら既に存在しているtableID=4が再度出来上がる
+	// TODO: tableIdを付ける規則は改めて修正する必要がある
+	//       でないと例えばtableIdが1, 2, 3, 4とあって2がクローズされた後にまたtableが追加されたら既に存在しているtableId=4が再度出来上がる
 	//       バグを仕込む可能性が高そうな箇所である
-	if clientReq.ID == 0 {
-		tableID := len(tm.Tables) + 1
-		tm.Tables = append(tm.Tables, table.NewTable(tableID, clientReq.PlayersNum))
+	if tableId == 0 {
+		tableId = len(tm.Tables) + 1
+		tm.Tables = append(tm.Tables, table.NewTable(tableId, playersNum))
 
-		// TODO: ここでクライアント側にtableIDを返す処理が必要
-		fmt.Fprintf(w, "Your tableID is %v\n", tableID)
+		// TODO: ここでクライアント側にtableIdを返す処理が必要
+		fmt.Fprintf(w, "Your tableId is %v\n", tableId)
 	} else if len(tm.Tables) > 0 {
-		if err = tm.setTableAsRequested(clientReq); err != nil {
+		if err = tm.setTableAsRequested(tableId, playersNum); err != nil {
 			fmt.Fprintln(w, "Requested ID does not exist")
 			return
 		}
@@ -91,6 +99,27 @@ func (tm *TournamentManager) ServeHTTP(w http.ResponseWriter, req *http.Request)
 	} else {
 		w.Write([]byte("No table is set yet\n"))
 		return
+	}
+}
+
+func (tm *TournamentManager) ServeHTTP(w http.ResponseWriter, httpReq *http.Request) {
+	// TDとDealerのAPIは統一するべきかよく考える必要がある
+	var err error
+	tm.RequestJson, err = parseJsonRequest(httpReq)
+	if err != nil {
+		fmt.Fprintln(w, "Failed to parse json request")
+		return
+	}
+
+	isTdRequest, err := tm.RequestJson.Get("IsTDRequest").Bool()
+	if err != nil {
+		fmt.Fprintln(w, "Failed to assert key \"IsTDRequest\"")
+	}
+
+	if isTdRequest == true {
+		//tm.handleTournamentDirectorRequest()
+	} else {
+		tm.handleDealerRequest(w)
 	}
 
 	// debug
